d24ae2f58cd9ad7f4827db90f74fe5b0e2dfc90b
Chris Clark <clarkcp@battelleecology.org>
2020-08-28 17:57:53 -0600

    sample-explore refactor download dialog

--- apps/sample-explorer/src/components/Presentations/SampleEventPresentation.jsx
+++ apps/sample-explorer/src/components/Presentations/SampleEventPresentation.jsx
@@ -1,69 +1,36 @@
-import React, { useState } from "react";
+import React from 'react';
 
-import Grid from "@material-ui/core/Grid";
-import Modal from "@material-ui/core/Modal";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 import DownloadIcon from '@material-ui/icons/SaveAlt';
 
 import Theme from 'portal-core-components/lib/components/Theme';
 
-import SampleInfoPresentation from "./SampleInfoPresentation";
-import DownloadSamplesPresentation from "./DownloadSamplesPresentation";
-import DataGrid from "../DataGrid/DataGrid";
+import SampleInfoPresentation from './SampleInfoPresentation';
+import DownloadSamplesPresentation from './DownloadSamplesPresentation';
+import DataGrid from '../DataGrid/DataGrid';
 
-import { smsFields } from "../../util/appUtil";
+import { smsFields } from '../../util/appUtil';
 
-import "font-awesome/css/font-awesome.min.css";
+import 'font-awesome/css/font-awesome.min.css';
 
-const fileDownload = require("js-file-download");
-const { Parser } = require("json2csv");
-
-const useStyles = makeStyles(theme => ({
-  paper: {
-    position: "absolute",
-    width: 400,
-    backgroundColor: theme.palette.background.paper,
-    borderRadius: "4px",
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 4),
-  },
-}));
+const fileDownload = require('js-file-download');
+const { Parser } = require('json2csv');
 
 const SampleEventPresentation = (props) => {
-  const { onPopupResetClick } = props;
-  const classes = useStyles(Theme);
-  const [displayPopup, setDisplayPopup] = useState(false);
-
-  const launchDownloadPopup = () => {
-    setDisplayPopup(true);
-  };
-  const closeDownloadPopup = () => {
-    setDisplayPopup(false);
-    return onPopupResetClick();
-  };
   const downloadPossibleSMSFields = () => {
-    let headers = ["SMS Field Name", "Description"];
+    let headers = ['SMS Field Name', 'Description'];
     let csvData = [];
     for (let [key, value] of smsFields) {
       let row = [];
-      row = Object.assign(row, { "SMS Field Name": key });
-      row = Object.assign(row, { "Description": value });
+      row = Object.assign(row, { 'SMS Field Name': key });
+      row = Object.assign(row, { 'Description': value });
       csvData.push(row);
     }
-
     let jsonParser = new Parser({ fields: headers });
     let csvResult = jsonParser.parse(csvData);
-    fileDownload(csvResult, "smsFieldNames.csv");
-  };
-
-  let modalDownloadStyle = {
-    width: 495,
-    margin: "0px auto",
-    position: "relative",
-    marginTop: "40px"
+    fileDownload(csvResult, 'smsFieldNames.csv');
   };
 
   return (
@@ -75,42 +42,24 @@ const SampleEventPresentation = (props) => {
         History of Sample Custody Events along with all Sample Management
         System (SMS) fields (taxon excepted) available for the focal sample tag.
       </Typography>
-      <div style={{ marginTop: Theme.spacing(3) }}>
-        <div style={{ marginBottom: Theme.spacing(3) }}>
-          <SampleInfoPresentation  {...props} />
-        </div>
-        <div style={{ marginBottom: Theme.spacing(3) }}>
-          <DataGrid {...props} />
-        </div>
-        <div className="event-download" style={{ marginBottom: Theme.spacing(3) }}>
-          <Grid container spacing={1}>
-            <Grid item xs={12} sm={5} style={{ maxWidth: "320px" }}>
-              <Button
-                style={{ marginRight: Theme.spacing(3), whiteSpace: 'nowrap' }}
-                variant="outlined"
-                color="primary"
-                onClick={downloadPossibleSMSFields}
-              >
-                Download Possible SMS Fields List
-                <DownloadIcon fontSize="small" style={{ marginLeft: Theme.spacing(1) }} />
-              </Button>
-            </Grid>
-            <Grid item xs={12} sm={3}>
-              <Button variant="contained" color="primary" onClick={launchDownloadPopup}>
-                Download Sample(s)
-                <DownloadIcon fontSize="small" style={{ marginLeft: Theme.spacing(1) }} />
-              </Button>
-            </Grid>
-          </Grid>
-        </div>
-        <Modal
-          open={displayPopup}
-          onClose={closeDownloadPopup}
+      <div style={{ margin: Theme.spacing(3, 0) }}>
+        <SampleInfoPresentation  {...props} />
+      </div>
+      <div style={{ marginBottom: Theme.spacing(3) }}>
+        <DataGrid {...props} />
+      </div>
+      <div style={{ marginBottom: Theme.spacing(3), display: 'flex', flexWrap: 'wrap' }}>
+        <Button
+          style={{ marginRight: Theme.spacing(3), whiteSpace: 'nowrap' }}
+          variant="outlined"
+          color="primary"
+          onClick={downloadPossibleSMSFields}
+          data-selenium="download-sms-fields-list-button"
         >
-          <div style={modalDownloadStyle} className={classes.paper}>
-            <DownloadSamplesPresentation {...props} />
-          </div>
-        </Modal>
+          Download Possible SMS Fields List
+          <DownloadIcon fontSize="small" style={{ marginLeft: Theme.spacing(1) }} />
+        </Button>
+        <DownloadSamplesPresentation {...props} />
       </div>
     </div>
   );

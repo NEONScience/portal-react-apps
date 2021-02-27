707e77c08ff249c9778c6cd620e362e9cb78dde3
jsampson <jsampson@battelleecology.org>
2021-02-26 19:49:03 -0700

    sample-explorer: integrate SMS field API endpoint, dialog display and download

--- apps/sample-explorer/src/components/Presentations/SampleEventPresentation.jsx
+++ apps/sample-explorer/src/components/Presentations/SampleEventPresentation.jsx
@@ -1,38 +1,17 @@
 import React from 'react';
 
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
-import DownloadIcon from '@material-ui/icons/SaveAlt';
 
 import Theme from 'portal-core-components/lib/components/Theme';
 
 import SampleInfoPresentation from './SampleInfoPresentation';
 import DownloadSamplesPresentation from './DownloadSamplesPresentation';
 import DataGrid from '../DataGrid/DataGrid';
-
-import { smsFields } from '../../util/appUtil';
+import SampleSmsFieldsDialog from './SampleSmsFieldsDialog';
 
 import 'font-awesome/css/font-awesome.min.css';
 
-const fileDownload = require('js-file-download');
-const { Parser } = require('json2csv');
-
 const SampleEventPresentation = (props) => {
-  const downloadPossibleSMSFields = () => {
-    let headers = ['SMS Field Name', 'Description'];
-    let csvData = [];
-    for (let [key, value] of smsFields) {
-      let row = [];
-      row = Object.assign(row, { 'SMS Field Name': key });
-      row = Object.assign(row, { 'Description': value });
-      csvData.push(row);
-    }
-    let jsonParser = new Parser({ fields: headers });
-    let csvResult = jsonParser.parse(csvData);
-    fileDownload(csvResult, 'smsFieldNames.csv');
-  };
-
   return (
     <div style={{ marginBottom: Theme.spacing(3) }} data-selenium="sample-events-section">
       <Typography variant="h4" gutterBottom>
@@ -46,20 +25,15 @@ const SampleEventPresentation = (props) => {
         <SampleInfoPresentation  {...props} />
       </div>
       <div style={{ marginBottom: Theme.spacing(3) }}>
-        <DataGrid {...props} />
+        <DataGrid 
+          columnDefs={props.tableDefinition}
+          rowData={props.tableData}
+          uuid={props.sampleUuid}
+        />
       </div>
       <div style={{ marginBottom: Theme.spacing(3), display: 'flex', flexWrap: 'wrap' }}>
-        <Button
-          style={{ margin: Theme.spacing(0, 3, 2, 0), whiteSpace: 'nowrap' }}
-          variant="outlined"
-          color="primary"
-          onClick={downloadPossibleSMSFields}
-          data-selenium="download-sms-fields-list-button"
-        >
-          Download Possible SMS Fields List
-          <DownloadIcon fontSize="small" style={{ marginLeft: Theme.spacing(1) }} />
-        </Button>
         <DownloadSamplesPresentation {...props} />
+        <SampleSmsFieldsDialog {...props} />
       </div>
     </div>
   );

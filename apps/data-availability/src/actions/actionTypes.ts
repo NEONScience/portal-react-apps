354c0957114fbf461d624b772b1cc81f0bdbc64f
Jeremy Sampson <jsampson@battelleecology.org>
2022-03-18 11:00:48 -0600

    prepare v1.9.0, portal-core-components v1.11.0 (#15)
    * data product citation component, core-components update
    
    * latest citation core-components
    
    * latest citation core-components
    
    * latest citation core-components
    
    * prepare v1.9.0, portal-core-components v1.11.0
    
    * prepare app releases

--- apps/data-availability/src/actions/actionTypes.ts
+++ apps/data-availability/src/actions/actionTypes.ts
@@ -1,7 +1,9 @@
 import { AsyncActionType } from 'portal-core-components/lib/types/asyncFlow';
+import { AppActionTypes } from './app';
 
 export type AppActionType = AsyncActionType;
 
 export type AnyActionType = (
   AppActionType
+  | AppActionTypes
 );

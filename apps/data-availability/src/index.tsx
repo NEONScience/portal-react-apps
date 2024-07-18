bc50e481c045d2380a9157204d283749087ec4d2
jsampson <jsampson@battelleecology.org>
2024-07-18 11:49:59 -0600

    bump deps, core components ref

--- apps/data-availability/src/index.tsx
+++ apps/data-availability/src/index.tsx
@@ -26,9 +26,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// eslint-disable-next-line no-console
-// reportWebVitals(console.log);

ae17b4e877c609612a8d50e8b3649fc1ccc5ec19
Sterling Michel <michel@battelleecology.org>
2025-09-08 15:14:39 -0600

    adding tooltip info

--- apps/explore-data-products/src/components/Filters/FilterTheme.jsx
+++ apps/explore-data-products/src/components/Filters/FilterTheme.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Tooltip from '@material-ui/core/Tooltip';
 import DataThemeIcon from 'portal-core-components/lib/components/DataThemeIcon';
 import Theme from 'portal-core-components/lib/components/Theme';
 
@@ -37,27 +38,32 @@ const FilterTheme = (props) => {
       showResetButton={filtersApplied.includes(filterKey)}
     >
       <ul>
-        {filterItems[filterKey].map((filterItem) => (
-          <li key={filterItem.value}>
-            <FilterCheckBox
-              name={(
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <div style={{ margin: Theme.spacing(0.5, 1, 0, 0) }}>
-                    <DataThemeIcon theme={filterItem.value} size={3} />
+        {filterItems[filterKey].map((filterItem) => {
+          const countTitle = `data products are a part of the ${filterItem.name} theme`;
+          return (
+            <li key={filterItem.value}>
+              <FilterCheckBox
+                name={(
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <div style={{ margin: Theme.spacing(0.5, 1, 0, 0) }}>
+                      <DataThemeIcon theme={filterItem.value} size={3} />
+                    </div>
+                    <Tooltip title={countTitle} arrow>
+                      <span>
+                        {filterItem.name}
+                      </span>
+                    </Tooltip>
                   </div>
-                  <span>
-                    {filterItem.name}
-                  </span>
-                </div>
-              )}
-              value={filterItem.value}
-              count={filterItem.count}
-              countTitle={`{n} data products are a part of the ${filterItem.name} theme`}
-              checked={filterValues[filterKey].includes(filterItem.value)}
-              {...checkboxProps}
-            />
-          </li>
-        ))}
+                )}
+                value={filterItem.value}
+                count={filterItem.count}
+                countTitle={`${filterItem.count} ${countTitle}`}
+                checked={filterValues[filterKey].includes(filterItem.value)}
+                {...checkboxProps}
+              />
+            </li>
+          );
+        })}
       </ul>
     </FilterBase>
   );

8d2d823a07ca213ed03f1906ad8c2704f58e3f18
Bryan Fair <fairb@battelleecology.org>
2026-03-31 07:53:51 -0600

    Data-product-detail: add theme provider

--- apps/data-product-detail/src/App.jsx
+++ apps/data-product-detail/src/App.jsx
@@ -1,16 +1,21 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react';
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 
 import NeonRouter from 'portal-core-components/lib/components/NeonRouter';
 
 import DataProductContext from './components/DataProductContext';
 import DataProductPage from './components/DataProductPage';
 
+const theme = createTheme();
+
 export default function App() {
   return (
     <NeonRouter disableRedirect cleanPath={false}>
       <DataProductContext.Provider>
-        <DataProductPage />
+        <ThemeProvider theme={theme}>
+          <DataProductPage />
+        </ThemeProvider>
       </DataProductContext.Provider>
     </NeonRouter>
   );

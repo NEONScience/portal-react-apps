e8fb8b219e969d0e72a4f8e0604debf0e5dd4a5f
Bryan Fair <fairb@battelleecology.org>
2026-04-01 07:49:58 -0600

    Prototype data: theme provider

--- apps/prototype-data/src/App.jsx
+++ apps/prototype-data/src/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 
 import NeonRouter from 'portal-core-components/lib/components/NeonRouter';
 
 import PrototypeContext from './PrototypeContext';
 import PrototypePage from './PrototypePage';
 
+const theme = createTheme();
+
 export default function App() {
   return (
     <NeonRouter disableRedirect cleanPath={false}>
       <PrototypeContext.Provider>
-        <PrototypePage />
+        <ThemeProvider theme={theme}>
+          <PrototypePage />
+        </ThemeProvider>
       </PrototypeContext.Provider>
     </NeonRouter>
   );

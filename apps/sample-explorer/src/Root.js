227cd0fb07ed4059a6d26269dbd576129c3d7921
Bryan Fair <fairb@battelleecology.org>
2026-04-01 11:32:22 -0600

    Sample Exp: Theme provider

--- apps/sample-explorer/src/Root.js
+++ apps/sample-explorer/src/Root.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 
 import NeonRouter from 'portal-core-components/lib/components/NeonRouter';
 
@@ -7,10 +8,14 @@ import App from "./components/App";
 
 import { configureInitialStore } from "./store/store";
 
+const theme = createTheme();
+
 const Root = () => (
   <Provider store={configureInitialStore()}>
     <NeonRouter cleanPath={false} disableRedirect>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </NeonRouter>
   </Provider>
 );

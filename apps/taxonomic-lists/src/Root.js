c24f5860064127fc060a29f6a5ebf7fdb48732a0
Bryan Fair <fairb@battelleecology.org>
2026-04-02 10:11:00 -0600

    Taxon List: Theme provider

--- apps/taxonomic-lists/src/Root.js
+++ apps/taxonomic-lists/src/Root.js
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux"
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 
 import NeonRouter from 'portal-core-components/lib/components/NeonRouter';
 
 import App from "./components/app/App";
 import { store } from "./store/store"
 
+const theme = createTheme();
+
 class Root extends Component {
 	render() {
 		return (
 			<Provider store={store}>
         <NeonRouter>
-          <App />
+			    <ThemeProvider theme={theme}>
+          	<App />
+		  	  </ThemeProvider>
         </NeonRouter>
 			</Provider>
 		);
